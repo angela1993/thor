161350dd21ebd0374e21f229d335d83c3c7f6ae6
angela1993 <andjelaa.petrovicc@gmail.com>
2020-02-12 20:28:04 +0100

    Add files via upload

---
+++ build/precache-manifest.8178733a82c5d83754bb19e7f0d63e99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "34e220b7ec77f2d3c52396a247c012a9",
+    "url": "/thor/static/media/naslovna2.34e220b7.jpg"
+  },
+  {
+    "revision": "ebd473e6427eb703c8b2358f3d58d5ef",
+    "url": "/thor/static/media/naslovna1.ebd473e6.jpg"
+  },
+  {
+    "revision": "93e265e47f2841be074c",
+    "url": "/thor/static/js/runtime~main.bc1f449b.js"
+  },
+  {
+    "revision": "1bb103ff4e05c1505e60",
+    "url": "/thor/static/js/main.0bdac705.chunk.js"
+  },
+  {
+    "revision": "543b7af494327395259c",
+    "url": "/thor/static/js/2.739e1563.chunk.js"
+  },
+  {
+    "revision": "1bb103ff4e05c1505e60",
+    "url": "/thor/static/css/main.1124ad06.chunk.css"
+  },
+  {
+    "revision": "543b7af494327395259c",
+    "url": "/thor/static/css/2.ee703703.chunk.css"
+  },
+  {
+    "revision": "5e6d756b41741a3f1e3a9d69de8ae504",
+    "url": "/thor/index.html"
+  }
+];
